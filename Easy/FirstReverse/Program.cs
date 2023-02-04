2463526c29ba577945d90e179042bc5b007b9162
Yordan Dimitrov <106671609+DimitrovYordan@users.noreply.github.com>
2023-02-04 15:23:54 +0200

    Add files via upload

---
+++ Easy/FirstReverse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FirstReverse
+{
+    class MainClass
+    {
+
+        public static string FirstReverse(string str)
+        {
+            //char[] charArray = str.ToCharArray();
+            //Array.Reverse(charArray);
+            //return new string(charArray);
+
+            string reversedWords = String.Empty;
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversedWords += str[i];
+            }
+
+            return reversedWords;
+        }
+
+        static void Main()
+        {
+            // keep this function call here
+            Console.WriteLine(FirstReverse(Console.ReadLine()));
+        }
+
+    }
+}
