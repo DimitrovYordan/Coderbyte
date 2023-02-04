e107da132577705309184a36ee3238958dc063f4
Yordan Dimitrov <106671609+DimitrovYordan@users.noreply.github.com>
2023-02-04 15:22:12 +0200

    Add files via upload

---
+++ Easy/FirstFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FirstFactorial
+{
+    class Program
+    {
+        public static int FirstFactorial(int num)
+        {
+            // code goes here  
+            int result = 1;
+            for (int i = num; i > 1; i--)
+            {
+                result *= i;
+            }
+            return result;
+
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(FirstFactorial(int.Parse(Console.ReadLine())));
+        }
+    }
+}
