3b337688152d6efa20d6f8108d3ea98ad60ec76a
Yordan Dimitrov <106671609+DimitrovYordan@users.noreply.github.com>
2023-03-27 10:04:30 +0300

    Add files via upload

---
+++ Easy/Longest Word/LongestWord/LongestWord/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LongestWord
+{
+    class Program
+    {
+        public static string LongestWord(string sen)
+        {
+            string[] current = sen.Split(" ");
+            string result = string.Empty;
+
+            for (int i = 0; i < current.Length; i++)
+            {
+                if (i >= result.Length)
+                {
+                    result = current[i];
+                }
+            }
+
+            // code goes here  
+            return result;
+        }
+
+        static void Main()
+        {
+            // keep this function call here
+            Console.WriteLine(LongestWord(Console.ReadLine()));
+        }
+    }
+}
